17431c57a0f1a45e4fc1583f989865369d125dfd
Jay <jaytestai123-svg@users.noreply.github.com>
2026-03-13 23:22:50 -0600

    Fix: use Render PORT env var for web server

--- web/server.js
+++ web/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.WEB_PORT || 3001;
+const PORT = process.env.PORT || process.env.WEB_PORT || 3001;
 
 // Public landing page
 app.use(express.static(path.join(__dirname, '../website')));
